aa239b56e6f4c58052ce8422f00b6b17a59c80ee
Dian Yordanov <dido.jordanov7@gmail.com>
2015-03-10 20:27:45 +0000

    done async task and sms sending with base64

--- app/src/main/java/com/xyengine/xelnectmobileuser/smsreciever/IncomingSms.java
+++ app/src/main/java/com/xyengine/xelnectmobileuser/smsreciever/IncomingSms.java
@@ -3,6 +3,7 @@ package com.xyengine.xelnectmobileuser.smsreciever;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
@@ -23,6 +24,7 @@ public class IncomingSms extends BroadcastReceiver {
     String senderNum;
     static String message;
     public static ArrayList<String> smsMessage;
+    public String pastSMSText;
 
     public void onReceive(Context context, Intent intent) {
 
@@ -43,14 +45,20 @@ public class IncomingSms extends BroadcastReceiver {
                         Log.v("ww", Integer.toString(msgs.length));
                         msgBody=msgBody+msgs[i].getMessageBody();
                     }
-                    MainActivity.updateView(MainActivity.timeView, "Message recieved on: " + IncomingSms.getCurrentTime());
-                    MainActivity.updateView(MainActivity.smsView, msgBody);
+                   new LongOperation(){
+                       @Override public void onPostExecute(String result)
+                       {
+                           doStuffWithIncomingSMS(result);
+                       }
+                   }.execute(msgBody);
+
                 }catch(Exception e){
 //                            Log.d("Exception caught",e.getMessage());
                 }
             }
         }
     }
+
         // Retrieves a map of extended data from the intent.
         /*final Bundle bundle = intent.getExtras();
 
@@ -86,13 +94,83 @@ public class IncomingSms extends BroadcastReceiver {
 
         }
     }*/
-    public static String getCurrentTime(){
+    public void doStuffWithIncomingSMS(String msgBody1){
+            MainActivity.updateView(MainActivity.timeView, "Message recieved on: " + getCurrentTime());
+            MainActivity.updateView(MainActivity.smsView, msgBody1);
+    }
+    public String getCurrentTime(){
         Calendar cal = Calendar.getInstance();
         int second = cal.get(Calendar.SECOND);
         int minute = cal.get(Calendar.MINUTE);
         int hour = cal.get(Calendar.HOUR);
         //.out.println( sdf.format(cal.getTime()) );
         return hour + ":" + minute + ":" + second;
+    }
+
+
+    private class LongOperation extends AsyncTask<String, Void ,String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+          /*  for (int i = 0; i < 5; i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Thread.interrupted();
+                }
+            }
+            return "Executed";*/
+
+            Log.e("",params[0]);
+            String stringTobeDecompressed = params[0].replace("Sent from your Twilio trial account - ","");
+            String stringTobeDecompressed1 = stringTobeDecompressed.replaceFirst(" ","");
+            Log.e("StringToBeDecompressed:","" + stringTobeDecompressed1);
+            //String stringBeingDecompressed = LZString.decompress(stringTobeDecompressed);
+
+            String decompressedMessage = null;
+            try {
+                decompressedMessage = LZString.decompressFromBase64(stringTobeDecompressed1);
+
+            Log.e("","" + decompressedMessage);
+            pastSMSText = decompressedMessage;
+            Log.e("","" + pastSMSText);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+           /* try {
+                String decompressedMessage = LZString.decompressFromUTF16(stringTobeDecompressed1);
+                Log.e("","" + decompressedMessage);
+                pastSMSText = decompressedMessage;
+                Log.e("","" + pastSMSText);
+            } catch (Exception e) {
+                Log.e("SmsReceiver", "Exception smsReceiver" +e);
+
+            }*/
+
+            //pastSMSText = LZString.decompress(pastSMSText);
+
+            //doStuffWithIncomingSMS(params[0]);
+           // doStuffWithIncomingSMS(pastSMSText);
+            return pastSMSText;
+        }
+
+        
+        protected String onPostExecute(String... result) {
+           // MainActivityTextView.setText(pastSMSText);
+           //  return null;// txt.setText(result);
+            // might want to change "executed" for the returned string passed
+            // into onPostExecute() but that is upto you
+        return pastSMSText;
+        }
+        @Override
+        protected void onPreExecute() {}
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+           // doStuffWithIncomingSMS(values.toString());
+        }
+
+
     }
     //test change
 /*    public static String getMessage(){
